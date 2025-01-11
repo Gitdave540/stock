9eeab5b9e849aa980a828f207dcd9e88a129b689
Gitdave540 <David.Palfrader@student.uibk.ac.at>
2025-01-11 14:41:58 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,46 +1,15 @@
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-        // erstellen einen Stock-Objekt
-        Stock aapl = new Stock("AAPL",
-                "Apple Inc.",
-                new BigDecimal("150.00"),
-                Exchange.NASDAQ,
-                Currency.USD);
-        Stock basf = new Stock("BASF",
-                "Basf Germany",
-                new BigDecimal(45.21),
-                Exchange.TSX,
-                Currency.EUR);
-
-        //erstellen eine StockPosition
-        StockPosition applePosition = new StockPosition(aapl, 100, new BigDecimal(151.45));
-        StockPosition basfPosition = new StockPosition(basf, 20, new BigDecimal(45.12));
-        StockPosition apple1Position = new StockPosition(aapl, 100, new BigDecimal(151.45));
-
-        // Create a list of StockPosition objects
-        List<StockPosition> stockPositions = new ArrayList<>();
-        stockPositions.add(applePosition);
-        stockPositions.add(basfPosition);
-        stockPositions.add(apple1Position);
-
-        //erstellen eines Portfolios
-        Portfolio myPortfolio = new Portfolio("Teck Portfolio", 2022,stockPositions);
-
-        System.out.println("Portfolio Name: " + myPortfolio.getName());
-        System.out.println("Initial Year: " + myPortfolio.getInitialYear());
-        System.out.println("Stocks in Portfolio:");
-
-        // for loop für die Ausgabe der einzelnen positionen
-        for (StockPosition stockPosition : stockPositions) {
-            System.out.println(stockPosition);
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
         }
-
     }
-
 }
